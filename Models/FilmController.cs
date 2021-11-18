b1fd2ca62dc809fd421b8deb6d0719ef5997256e
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-18 20:37:43 +0100

    connection with database

---
+++ Models/FilmController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB.Models
+{
+    public class FilmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
